cd017ac0b0908de786a1a3b8f1f914975e6bc240
erich.gluck <erichgluck@gmail.com>
2020-06-09 08:26:59 +0200

    Added basic movable character

---
+++ Perfection/Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject
+{
+    public float maxSpeed = 7;
+    public float jumpTakeoffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeoffSpeed;
+        }
+        else if(Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y *= 0.5f;
+            }
+        }
+
+        targetVelocity = move * maxSpeed;
+    }
+}
