64c8d28f4196809be5ed37e4b6f6b383cc6e954c
erich.gluck <erichgluck@gmail.com>
2021-01-17 15:15:37 +0100

    Added basic object manipulation through use of fixed joints
    Currently the biggest issue is when turning, the player and object being carried seem to collide with eachother, leading to glitchy spinning motions

---
+++ Perfection/Assets/Manipulation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manipulation : MonoBehaviour
+{
+    public string InteractButton = "J1_X";
+
+    private void Update()
+    {
+        if (!Input.GetButton(InteractButton))
+        {
+            Component[] joints = gameObject.GetComponents<FixedJoint2D>() as Component[];
+            foreach (Component joint in joints)
+            {
+                Destroy(joint as FixedJoint2D);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetButtonDown(InteractButton) && collision.gameObject.GetComponent("Rigidbody2D"))
+        {
+            FixedJoint2D joint = gameObject.AddComponent<FixedJoint2D>() as FixedJoint2D;
+            joint.connectedBody = (Rigidbody2D)collision.gameObject.GetComponent("Rigidbody2D");
+            joint.enableCollision = false;
+        }
+    }
+}
--- Perfection/Assets/Scripts/PlayerController.cs
+++ Perfection/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D body;
+    
     public float Speed;
+    public float Torque;
     public float BoostSpeedMultiplier;
     public string DashButton = "J1_A";
     public string HorizontalAxis = "J1_Horizontal";
     public string VerticalAxis = "J1_Vertical";
-    
-    
+
+    public float BodyAngle;
+    public float TargetAngle;
+    public float Diff;
+    public float Diff2;
+
+    private Rigidbody2D body;
     private bool Boosting = false;
     private Vector2 BoostSpeed = new Vector2(0,0);
 
@@ -22,13 +28,22 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Boost();
 
         Vector2 currentPosition = transform.position;
         body.AddForce(body.mass * Time.deltaTime * (BoostSpeed + new Vector2(Input.GetAxis(HorizontalAxis) * Speed, Input.GetAxis(VerticalAxis) * Speed)));
-        body.velocity /= 2;
+        if (body.velocity.magnitude > 0.5)
+        {
+            BodyAngle = body.rotation;
+            Diff = Vector2.SignedAngle(Vector2.right, body.velocity) - BodyAngle;
+
+            Vector2 BodyAngleAsVector = (Vector2)(Quaternion.Euler(0, 0, BodyAngle) * Vector2.right);
+            Diff2 = Vector2.SignedAngle(BodyAngleAsVector, body.velocity);
+
+            body.AddTorque(Torque * Time.deltaTime * Diff2, ForceMode2D.Impulse);
+        }
     }
 
     void Boost()
